2d455154a824d6a9e6998073d29a952940832ffe
wilfredpine <only.master.red@gmail.com>
2023-06-11 22:45:14 +0800

    updated index page view into tabular view

--- app/models/Student_model.php
+++ app/models/Student_model.php
@@ -4,16 +4,21 @@ defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 class Student_model extends Model
 {
 
-    public function students($studid)
+    public function students($studid, $limit=20, $offset=1)
     {
         if ($studid == null) {
-            return $this->db->table('student')->select('*')->where('CORID=?', [2])->get_all();
+            return $this->db->table('student')
+                            ->select('*')
+                            ->where('CORID=?', [2])
+                            ->limit($limit)
+                            ->offset($offset)
+                            ->get_all();
         } else {
             //use only for filtering
             return $this->db->table('student')
-                ->select('*')
-                ->where('CORID=? AND STUDID=?', [2, $studid])
-                ->get_all();
+                            ->select('*')
+                            ->where('CORID=? AND STUDID=?', [2, $studid])
+                            ->get_all();
         }
     }
 
--- app/views/home_view.php
+++ app/views/home_view.php
@@ -7,21 +7,27 @@ defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 <header class="app-header fixed-top">
 
 	<div class="app-header-inner">
-		<div class="container-fluid py-2">
-			<div class="app-header-content">
-				<div class="row justify-content-between align-items-center">
+		<div class="container-fluid py-2 ps-0 ms-0">
+			
+			<div class="app-header-content ps-0 ms-0">
+				
+				<div class="row justify-content-between align-items-center ps-0 ms-0">
 
 					<div class="search-mobile-trigger d-sm-none col">
 						<i class="search-mobile-trigger-icon fas fa-search"></i>
 					</div><!--//col-->
-					<div class="app-search-box col">
-						<form class="app-search-form">
-							<input type="text" placeholder="Search..." name="search" class="form-control search-input">
-							<button type="submit" class="btn search-btn btn-primary" value="Search"><i class="fas fa-search"></i></button>
+
+					
+					<div class="app-search-box col ps-0 ms-0">
+						<form class="app-search-form ps-0 ms-0">
+							<h1 class="app-page-title mb-0" style="margin-left: -150px">REALTIME UPDATES</h1>
+							
 						</form>
 					</div><!--//app-search-box-->
 
 					<div class="app-utilities col-auto pe-5">
+
+						<small class="me-5">Results as of <?php echo date_today(); ?></small>
 
 						<div class="app-utility-item pe-5">
 							<a href="<?php echo BASE_URL; ?>auth/" title="Login">
@@ -49,96 +55,94 @@ defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 
 	<div class="app-content pt-3 p-md-3 p-lg-4">
 		<div class="container-xl">
-			<div class="row g-3 mb-4 align-items-center justify-content-between">
-				<div class="col-auto">
-					<h1 class="app-page-title mb-0">REALTIME UPDATES</h1><small>Results</small>
-				</div>
-				<div class="col-auto">
-					<div class="page-utilities">
-						<div class="row g-2 justify-content-start justify-content-md-end align-items-center">
-							<div class="col-auto">
-								<form class="docs-search-form row gx-1 align-items-center">
-									<div class="col-auto">
-										<input type="text" id="search-docs" name="searchdocs" class="form-control search-docs" placeholder="Search">
-									</div>
-									<div class="col-auto">
-										<button type="submit" class="btn app-btn-secondary">Search</button>
-									</div>
-								</form>
-
-							</div><!--//col-->
-							<div class="col-auto">
-
-								<select class="form-select w-auto">
-									<option selected="" value="option-1">All</option>
-									<option value="option-2">Text file</option>
-									<option value="option-3">Image</option>
-									<option value="option-4">Spreadsheet</option>
-									<option value="option-5">Presentation</option>
-									<option value="option-6">Zip file</option>
-
-								</select>
-							</div>
-							<div class="col-auto">
-								<a class="btn app-btn-primary" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-upload me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
-										<path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"></path>
-									</svg>Upload File</a>
-							</div>
-						</div><!--//row-->
-					</div><!--//table-utilities-->
-				</div><!--//col-auto-->
-			</div><!--//row-->
-
-			<!-- <div class="row g-3 mb-4 align-items-center justify-content-between">
-				    <div class="col-auto">
-			            <h1 class="app-page-title mb-0">Governor</h1>
-				    </div>
-				</div> -->
-
+			
 			<div class="row g-4">
 
-				<?php foreach ($candidates as $candidate) { ?>
-					<?php if ($candidate['picture'] == '') {
-						$candidate['picture'] = 'no-avatar.png';
-					} ?>
-					<div class="col-6 col-md-4 col-xl-3 col-xxl-2">
-						<div class="app-card app-card-doc shadow-sm h-100">
-							<div class="app-card-thumb-holder p-3">
-								<div class="app-card-thumb">
-									<img class="thumb-image" src="<?php echo BASE_URL; ?>uploads/profile/<?php echo $candidate['picture']; ?>" alt="Candidate Picture">
-								</div>
-								<!-- <span class="badge bg-success">Top 1</span> -->
-								<a class="app-card-link-mask" href="#file-link"></a>
-							</div>
-							<div class="app-card-body p-3 has-card-actions">
 
-								<h4 class="app-doc-title truncate mb-0"><a href="#file-link"><?php echo $candidate['full_name']; ?></a></h4>
-								<div class="app-doc-meta">
-									<ul class="list-unstyled mb-0">
-										<li><span>Position:</span> <?php echo $candidate['position']; ?></li>
-										<li><span class="text-muted">Vote:</span> <?php echo vote_count($candidate['id']); ?></li>
-										<li><span class="text-muted">Updated:</span> 3 mins ago</li>
-									</ul>
-								</div><!--//app-doc-meta-->
-							</div><!--//app-card-body-->
-						</div><!--//app-card-->
-					</div><!--//col-->
-				<?php } ?>
+					
+						<div class="app-card app-card-orders-table shadow-sm mb-5">
+						    <div class="app-card-body">
+							    <div class="table-responsive">
+							        <table class="table app-table-hover mb-0 text-left">
+										<thead>
+											<tr>
+												<th class="cell">Position</th>
+												<th class="cell">Name</th>
+												
+												<!-- <th class="cell">Year &amp; Section</th> -->
+												<th class="cell">Vote</th>
+											</tr>
+										</thead>
+										<tbody>
+
+											<?php foreach ($candidates as $candidate) { ?>
+											<?php //if ($candidate['picture'] == '') {
+												//$candidate['picture'] = 'no-avatar.png';
+											//} ?>
+											
+												<tr style="margin:0px;padding:0px; padding-left: 20px;
+												<?php 
+												switch($candidate['position'])
+												{
+													case "Governor":
+														echo "background-color: rgb(37, 150, 190, .30);";
+														break;
+													case "Vice Governor":
+														echo "background-color: rgb(234,182,118, .30);";
+														break;
+													case "Secretary":
+														echo "background-color: rgb(30, 195, 10, .30);";
+														break;
+													case "Treasurer":
+														echo "background-color: rgb(195, 10, 13, .30);";
+														break;
+													case "Auditor":
+														echo "background-color: rgb(195, 152, 10, .30);";
+														break;
+													case "Board Member 1 (SPORTS, CULTURE AND ARTS)":
+														echo "background-color: rgb(207, 255, 0, .30);";
+														break;
+													
+													case "Board Member 2 (RESEARCH AND EXTENSION)":
+														echo "background-color: rgb(255, 0, 216, .30);";
+														break;
+													case "Board Member 3 (COMM/PUB MATS)":
+														echo "background-color: rgb(0, 46, 255, .30);";
+														break;
+													case "Board Member 4 (DOCU AND EVALUATION)":
+														echo "background-color: rgb(0, 255, 232, .30);";
+														break;
+													case "Board Member 5 (IGP)":
+														echo "background-color: rgb(7, 7, 7, .30);";
+														break;
+												} ?>
+												">
+													
+													<td style="margin:0px 0px 0px 20px;padding:0px" class="cell"> &nbsp;&nbsp;&nbsp; <?php echo $candidate['position']; ?></td>
+
+													<td style="margin:0px;padding:0px" class="cell"><span class="truncate"><?php echo $candidate['full_name']; ?></span></td>
+
+													<!-- <td style="margin:0px;padding:0px" class="cell"><span class="note"><?php echo $candidate['year_level']; ?>-<?php echo $candidate['section']; ?></span></td> -->
+
+													<td style="margin:0px;padding:0px" class="cell">
+														<!-- <span class="badge bg-success"></span> -->
+														<?php echo vote_count($candidate['id']); ?>
+													</td>
+												</tr>
+
+											<?php } ?>
+
+													
+										</tbody>
+									</table>
+						        </div><!--//table-responsive-->
+						       
+						    </div><!--//app-card-body-->		
+						</div>
+
+				
 			</div><!--//row-->
-			<nav class="app-pagination mt-5">
-				<ul class="pagination justify-content-center">
-					<li class="page-item disabled">
-						<a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-					</li>
-					<li class="page-item active"><a class="page-link" href="#">1</a></li>
-					<li class="page-item"><a class="page-link" href="#">2</a></li>
-					<li class="page-item"><a class="page-link" href="#">3</a></li>
-					<li class="page-item">
-						<a class="page-link" href="#">Next</a>
-					</li>
-				</ul>
-			</nav><!--//app-pagination-->
+			
 		</div><!--//container-fluid-->
 	</div>
 
